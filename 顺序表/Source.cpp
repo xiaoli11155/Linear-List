a6b872b3d12b64670e230cd70939f0bcf0c80c63
xiaoli <ckr2006@sjtu.edu.cn>
2025-01-19 16:31:18 +0800

    change

--- 顺序表/Source.cpp
+++ 顺序表/Source.cpp
@@ -1,4 +1,8 @@
 #include<list>
+#include<iostream>
+using namespace std;
+
+
 template<class elemType>
 class seqList :public list<elemType>
 {
@@ -7,6 +11,7 @@ private:
 	int currentLength;
 	int maxSize;
 	void doubleSpace();
+
 public:
 	seqList(int initSize = 10);
 	~seqList();
@@ -37,6 +42,51 @@ int seqList<elemType>::length()const {
 template <class elemType>
 elemType seqList<elemType>::visit(int i)const {
 	return data[i];
+}
+
+template <class elemType>
+void seqList<elemType>::traverse()const {
+	cout << endl;
+	for (int i = 0; i < currentLength; i++) {
+		cout << data[i] << ' ';
+	}
+}
+
+
+template <class elemType>
+seqList<elemType>::seqList(int initSize) {
+	data = new elemType[initSize];
+	maxSize = initSize;
+	currentLength = 0;
+}
+
+template <class elemType>
+int  seqList<elemType>::search(const elemType& x)const {
+	for (int i = 0; i < currentLength && data[i] != x; i++);
+	if (i == currentLength)return -1;
+	else return i;
+}
+
+template <class elemType>
+void seqList<elemType>::doubleSpace() {
+	elemType* tmp = data;
+	maxSize *= 2;
+	data = new elemType[maxSize];
+	for (int i = 0; i < currentLength; i++) {
+		data[i] = tmp[i];
+	}
+	delete[]tmp;
+}
+
+template <class elemType>
+void seqList<elemType>::insert(int i, const elemType& x) {
+	if (currentLength == maxSize)
+		doubleSpace();
+	for (int j = currentLength; j > i; i--) {
+		data[j] = data[j - 1];
+	}
+	data[i] = x;
+	currentLength++;
 }
 
 
