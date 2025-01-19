3e34d1b1d784418799041ea052409b15578e2af0
xiaoli <ckr2006@sjtu.edu.cn>
2025-01-20 00:02:17 +0800

    change

--- 单链表/sLinkList.cpp
+++ 单链表/sLinkList.cpp
@@ -1,20 +1,17 @@
 #include "sLinkList.h"
 
-// 构造函数
 template <class elemType>
 sLinkList<elemType>::sLinkList() {
     head = new node;
     currentLength = 0;
 }
 
-// 析构函数
 template <class elemType>
 sLinkList<elemType>::~sLinkList() {
     clear();
     delete head;
 }
 
-// 清空链表
 template <class elemType>
 void sLinkList<elemType>::clear() {
     node* p = head->next;
@@ -28,13 +25,11 @@ void sLinkList<elemType>::clear() {
     currentLength = 0;
 }
 
-// 返回链表长度
 template <class elemType>
 int sLinkList<elemType>::length() const {
     return currentLength;
 }
 
-// 返回第 i 个结点的地址
 template <class elemType>
 typename sLinkList<elemType>::node* sLinkList<elemType>::move(int i) const {
     node* p = head;
@@ -45,7 +40,6 @@ typename sLinkList<elemType>::node* sLinkList<elemType>::move(int i) const {
     return p;
 }
 
-// 在位置 i 插入元素 x
 template <class elemType>
 void sLinkList<elemType>::insert(int i, const elemType& x) {
     node* pos = move(i - 1);
@@ -53,7 +47,6 @@ void sLinkList<elemType>::insert(int i, const elemType& x) {
     currentLength++;
 }
 
-// 删除位置 i 的元素
 template <class elemType>
 void sLinkList<elemType>::remove(int i) {
     node* pos = move(i - 1);
@@ -63,7 +56,6 @@ void sLinkList<elemType>::remove(int i) {
     currentLength--;
 }
 
-// 查找元素 x
 template <class elemType>
 int sLinkList<elemType>::search(const elemType& x) const {
     node* p = head->next;
@@ -76,13 +68,12 @@ int sLinkList<elemType>::search(const elemType& x) const {
     else return i;
 }
 
-// 访问位置 i 的元素
+
 template <class elemType>
 elemType sLinkList<elemType>::visit(int i) const {
     return move(i)->data;
 }
 
-// 遍历链表
 template <class elemType>
 void sLinkList<elemType>::traverse() const {
     node* p = head->next;
